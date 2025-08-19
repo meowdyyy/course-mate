c331782ff294fad7936dfe86938f4202571e3f84
Zarif Ashrafee <35692115+ashrafee-z@users.noreply.github.com>
2025-08-20 02:17:01 +0600

    Delete routes directory

--- routes/commentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/commentController');
-
-router.post('/comment', commentController.addComment);
-router.get('/comments/:resourceId', commentController.getComments);
-
-module.exports = router;
--- routes/ratingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ratingController = require('../controllers/ratingController');
-
-router.post('/rate', ratingController.addRating);
-router.get('/ratings/:resourceId', ratingController.getRatings);
-
-module.exports = router;
